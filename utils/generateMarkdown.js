0d7875b0c56c5d9fb0e069fbf4e8329be78ac686
Gustavo Murillo-Gonzalez <gmicho015@gmail.com>
2023-05-30 23:27:49 -0400

    Added license functions to generatemarkdown plus the generatemarkdown fuction with table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,101 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  let badgeLabel = license.replace(" ", "&ensp;");
+  return `
+  [![Generic badge](https://img.shields.io/badge/License-${badgeLabel}-green.svg)](${renderLicenseLink(license)})
+  `
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function renderLicenseLink(license) {
+  const linkUrl = encodeURIComponent(license);
+  return `https://choosealicense.com/licenses/${linkUrl}/.`;
+}
 
+
+function renderLicenseSection(license) {
+
+  if (license === "None") {
+    return "";
+  } else {
+
+    return `
+  ### Licensing 
+  ${license}
+  ${renderLicenseBadge(license)}
+  `
+}
+};
+
+
+function renderLinkSection(siteLink) {
+
+  if (!siteLink) {
+    return "";
+  } else {
+
+    return `
+  ### Live Site 
+  [Click to see the live site!](${siteLink})
+  `
+}
+};
+
+
+function renderDemo(demoLink) {
+
+  if (!demoLink) {
+    return "";
+  } else {
+
+    return `
+  ### Demo 
+  ![Demo](${demoLink})
+  `
+}
+};
+
+
+
+function generateMarkdown({ github, licenseChoice, confirmLiveLink, liveSiteLink, siteDemoLink, projectTitle, description, installationInstructions, usageInstructions, previewLink, contributionInstructions, testInstructions, questionsEmail }) {
+  return `
+# ${projectTitle}
+
+## Table of Contents
+- [Description](#project-description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Preview](#preview)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Questions](#questions)
+
+## Project Description
+${description}
+${renderLicenseSection(licenseChoice)} 
+${renderLinkSection(liveSiteLink)}
+${renderDemo(siteDemoLink)}
+
+## Installation 
+${installationInstructions}
+
+## Usage 
+${usageInstructions}
+
+## Preview
+![Preview](${previewLink})
+
+## Contribution
+${contributionInstructions}
+
+## Testing
+${testInstructions}
+
+## Questions
+Reach out to the repo owner, [${github}](https://github.com/${github}) at ${questionsEmail}.
 `;
 }
 
 module.exports = generateMarkdown;
+
+
