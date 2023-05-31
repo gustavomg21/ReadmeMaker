41f82fbb4c18d328cd1fe428ec3766bade31b790
Gustavo Murillo-Gonzalez <gmicho015@gmail.com>
2023-05-31 19:28:21 -0400

    Included the necessary packages for the aplication fs and inquirer and link to markdown

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
